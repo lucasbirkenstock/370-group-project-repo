25adce5fc3af6c709f4107aee3d478974e1fa98d
Lbirk <lucasbirkenstock@protonmail.com>
2023-09-23 00:18:59 -0700

    Starting connection file

---
+++ brainbit_connection.py
@@ -0,0 +1,12 @@
+import flask
+import neurosdk
+from neurosdk.scanner import Scanner
+from neurosdk.sensor import Sensor
+from neurosdk.brainbit_sensor import BrainBitSensor
+from neurosdk.cmn_types import *
+
+
+# https://sdk.brainbit.com/
+scanner = Scanner([SensorFamily.SensorLEBrainBit])
+# sensor = scanner.create_sensor(sensInfo)
+
