698892b2d23c03e3c72d46f5143854321039b981
Lbirk <lucasbirkenstock@protonmail.com>
2023-09-25 13:17:14 -0700

    Wrote a couple api calls to SQL database, working

--- brainbit_api.py
+++ brainbit_api.py
@@ -10,10 +10,25 @@ password="password"
 )
 
 theCursor = conn.cursor() 
-theCursor.execute("INSERT INTO example (stupid) VALUES(5)")
-theCursor.execute("SELECT * FROM example")
-theCursor.fetchone() 
+
+# Return serial number from searching_device table
+# TODO Make it print/store value in a variable
+def getSerialNumber():
+    theCursor.execute("SELECT SerialNumber FROM searching_device")
+
+# Write to data table receiving_resistance as described here: https://pypi.org/project/pyneurosdk2/#receiving-signal
+def writeToReceivingResistance(iO1, iO2, iT3, iT4):
+    theCursor.execute("""INSERT INTO receiving_resistance ("O1", "O2", "T3", "T4") VALUES(%s, %s, %s, %s);
+                      """, 
+                      (iO1, iO2, iT3, iT4))
+
+# Testing above function: functional
+writeToReceivingResistance(0.1, 0.2, 0.3, 0.4)
+
+# Ensure effects of any data manipulation are not lost
 conn.commit()
+
+# Close connections to the cursor and connection
 theCursor.close()
 conn.close()
 
