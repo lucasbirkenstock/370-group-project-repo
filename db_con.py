7386a9df849b649f473e527a72cace15058e2bba
Lbirk <lucasbirkenstock@protonmail.com>
2023-10-11 13:59:55 -0700

    Started db work

--- db_con.py
+++ db_con.py
@@ -8,24 +8,28 @@ import sqlite3
 def get_db():
     #Postgres
     #return psycopg2.connect(host="localhost", dbname="authme" , user="loki", password="4prez")
-    return sqlite3.connect("local_data_base")
+    return sqlite3.connect("local_data_base.db")
 
 def get_db_instance():  
     db  = get_db()
-    cur  = db.cursor( )
+    theCursor  = db.cursor()
 
-    return db, cur 
+    return db, theCursor 
 
-
+def video_db_setup():
+    db, theCursor = get_db_instance()
+    theCursor.execute("CREATE TABLE example (asdf int, abc int)")
 
 if __name__ == "__main__":
-    db, cur = get_db_instance()
+    db, theCursor = get_db_instance()
 
-    cur.execute("select * from users")
-    for r in cur.fetchall():
-        print(r)
+    video_db_setup()
+    theCursor.execute("INSERT INTO example (asdf, abc) VALUES (5, 2)")
+    
+    #cur.execute("select * from users")
+    for r in theCursor.fetchall():
+       print(r)
 
-    cur.execute("create table music ( song_name varchar(255), rating int);")
     db.commit()
 
 
