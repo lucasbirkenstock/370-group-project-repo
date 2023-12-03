631244350f6d482606740b8bc09a2fbabb8d5cb0
Lucas Birkenstock <lucasbirkenstock@protonmail.com>
2023-12-02 23:26:48 -0800

    Small changes

--- tools/login.py
+++ tools/login.py
@@ -1,6 +1,9 @@
 import sqlite3
 import hashlib
 
+# Installing an sqlite extension is reccommended for working on this
+
+
 # Set up connection to sqlite database
 connection = sqlite3.connect("usercredentials.db")
 
@@ -21,3 +24,9 @@ CREATE TABLE IF NOT EXISTS usercredentials(
 # Example credentials
 # In database, store username and a hashed password converted to hex
 exampleUsername1, examplePassword1 = "lucas123", hashlib.sha256("password123".encode()).hexdigest()
+
+# Insert sample data
+theCursor.execute("INSERT INTO usercredentials (username, password) VALUES (?, ?)", (exampleUsername1, examplePassword1))
+
+# Actually send/save the data
+connection.commit()
