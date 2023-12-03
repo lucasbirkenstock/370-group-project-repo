811f7877ae196642a626ba03839f9f1cf7581336
Lucas Birkenstock <lucasbirkenstock@protonmail.com>
2023-12-02 23:20:30 -0800

    Starting login system

---
+++ tools/login.py
@@ -0,0 +1,23 @@
+import sqlite3
+import hashlib
+
+# Set up connection to sqlite database
+connection = sqlite3.connect("usercredentials.db")
+
+# Set up cursor
+theCursor = connection.cursor()
+
+# Initialize database if not already existing
+# Three columns: id, user, password
+
+theCursor.execute("""
+CREATE TABLE IF NOT EXISTS usercredentials(
+                  id INTEGER PRIMARY KEY,
+                  username VARCHAR(255) NOT NULL, 
+                  password VARCHAR(255) NOT NULL
+)
+""")
+
+# Example credentials
+# In database, store username and a hashed password converted to hex
+exampleUsername1, examplePassword1 = "lucas123", hashlib.sha256("password123".encode()).hexdigest()
