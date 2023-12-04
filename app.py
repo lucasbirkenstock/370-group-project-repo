7e73945e2c1dc909537d7b9c36a88685c39653d8
Lucas Birkenstock <lucasbirkenstock@protonmail.com>
2023-12-04 12:26:22 -0800

    logging more progress

--- app.py
+++ app.py
@@ -1,120 +1,77 @@
-from flask import Flask,render_template,request, redirect, url_for, g
+from flask import Flask, render_template, request, redirect, url_for, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
-#import jwt
-
 import sys
 import datetime
-#import bcrypt
 import traceback
-import socket 
+import socket
 
 from tools.eeg import get_head_band_sensor_object
-
-
 from db_con import get_db_instance, get_db
-
-#from tools.token_required import token_required
-
-#used if you want to store your secrets in the aws valut
-#from tools.get_aws_secrets import get_secrets
-
-#  This code sets up a web application with specific API endpoints for executing functions
 
 from tools.logging import logger
 
 ERROR_MSG = "Ooops.. Didn't work!"
 
-
-#Create our app
 app = Flask(__name__)
-#add in flask json
 FlaskJSON(app)
 
-#g is flask for a global var storage 
 def init_new_env():
-    #To connect to DB
     if 'db' not in g:
         g.db = get_db()
 
     if 'hb' not in g:
         g.hb = get_head_band_sensor_object()
 
-    #g.secrets = get_secrets()
-    #g.sms_client = get_sms_client()
-
-#This gets executed by default by the browser if no page is specified
-#So.. we redirect to the endpoint we want to load the base page
-@app.route('/') #endpoint
+@app.route('/')
 def index():
     return redirect('/static/index.html')
 
-
-@app.route("/secure_api/<proc_name>",methods=['GET', 'POST'])
-#@token_required
+@app.route("/secure_api/<proc_name>", methods=['GET', 'POST'])
 def exec_secure_proc(proc_name):
     logger.debug(f"Secure Call to {proc_name}")
-
-    #setup the env
     init_new_env()
-
-    #see if we can execute it..
     resp = ""
     try:
-        fn = getattr(__import__('secure_calls.'+proc_name), proc_name)
+        fn = getattr(__import__('secure_calls.' + proc_name), proc_name)
         resp = fn.handle_request()
     except Exception as err:
         ex_data = str(Exception) + '\n'
         ex_data = ex_data + str(err) + '\n'
         ex_data = ex_data + traceback.format_exc()
         logger.error(ex_data)
-        return json_response(status_=500 ,data=ERROR_MSG)
-
+        return json_response(status_=500, data=ERROR_MSG)
     return resp
 
-
-
-
-
-@app.route("/open_api/<proc_name>",methods=['GET', 'POST'])
+@app.route("/open_api/<proc_name>", methods=['GET', 'POST'])
 def exec_proc(proc_name):
     logger.debug(f"Call to {proc_name}")
-
-    #setup the env
     init_new_env()
-
-    #see if we can execute it..
     resp = ""
     try:
-        fn = getattr(__import__('open_calls.'+proc_name), proc_name)
+        fn = getattr(__import__('open_calls.' + proc_name), proc_name)
         resp = fn.handle_request()
     except Exception as err:
         ex_data = str(Exception) + '\n'
         ex_data = ex_data + str(err) + '\n'
         ex_data = ex_data + traceback.format_exc()
         logger.error(ex_data)
-        return json_response(status_=500 ,data=ERROR_MSG)
-
+        return json_response(status_=500, data=ERROR_MSG)
     return resp
 
-
+# Add this block at the end of app.py
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 80))
 
 @app.route('/login', methods=['POST'])
 def login():
     try:
-        # Send username and password to the server
         client.send(request.form['username'].encode())
-        client.recv(1024)  # Receive the password prompt
+        client.recv(1024)  
         client.send(request.form['password'].encode())
-
-        # Receive and return the result from the server
         result = client.recv(1024).decode()
         return result
-
     except Exception as e:
         return f"Error: {e}"
-
