3555aeee73fdeb32e2fa69f7d5288cdb9884c003
Lbirk <lucasbirkenstock@protonmail.com>
2023-12-05 23:06:42 -0800

    Potentially fixed pipeline

--- tools/eeg.py
+++ tools/eeg.py
@@ -2,9 +2,12 @@ from neurosdk.scanner import Scanner
 from neurosdk.sensor import Sensor
 from neurosdk.brainbit_sensor import BrainBitSensor
 from neurosdk.cmn_types import *
-
+from time import sleep
 from tools.logging import logger
 from em_st_artifacts import emotional_math 
+# support classes in em_st_artifacts > utils 
+from em_st_artifacts.utils import support_classes
+from em_st_artifacts.utils import lib_settings
 
 from .lib_emotions_pipeline import make_pipeline_object
 
@@ -14,16 +17,37 @@ def on_sensor_state_changed(sensor, state):
     logger.debug('Sensor {0} is {1}'.format(sensor.Name, state))
 
 # Variable to store brain data
-my_data = []
+#my_data = []
+
+thePipeline = make_pipeline_object()
 
 # This function CONSTANTLY invokes when the headband is connected and the video is playing
 def on_brain_bit_signal_data_received(sensor, data):
     # Copy the information from 'data' to the global variable 'my_data'
-    global my_data
+    #global my_data
     #logger.debug(data)
-    my_data.append(data)
+    #my_data.append(data)
     #print("test123")
-    print(my_data)
+    #print(my_data)
+
+    raw_channels = [support_classes.RawChannels]
+    for sample in data: 
+        left_bipolar = sample.T3-sample.O1
+        right_bipolar = sample.T4-sample.O2
+        raw_channels.append(support_classes.RawChannels(left_bipolar, right_bipolar))
+
+    thePipeline.push_data(raw_channels)
+    thePipeline.process_data_arr()
+
+    if not thePipeline.calibration_finished():
+        print(f'Artifacted: {thePipeline.is_both_sides_artifacted()}')
+        print(f'Calibration percents: {thePipeline.get_calibration_percents()}')
+    else :
+        print(f'Artifacted: {thePipeline.is_artifacted_sequence()}')
+        print(f'Mental data: {thePipeline.read_mental_data_arr()}')
+        print(f'Spectral data: {thePipeline.read_spectral_data_percents_arr()}')
+    print(data)
+
 
 logger.debug("Create Headband Scanner")
 gl_scanner = Scanner([SensorFamily.SensorLEBrainBit])
@@ -49,10 +73,14 @@ def sensorFound(scanner, sensors):
 
         
         #################### Pipeline stuff: may need to be moved elsewhere later. ####################
-        thePipeline = make_pipeline_object()
-        # There is a problem with push_data: investigate later
-        # thePipeline.push_data(my_data)
-        # thePipeline.process_data_arr()
+        
+        if gl_sensor.is_supported_command(SensorCommand.StartSignal()):
+            gl_sensor.exec_command(SensorCommand.StartSignal)
+            print("Start signal")
+            thePipeline.start_calibration()
+            sleep(120)
+            gl_sensor.exec_command(SensorCommand.StopSignal)
+
         
         
         break
